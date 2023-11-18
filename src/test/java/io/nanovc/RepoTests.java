d895804f11b4b4166ead08d028cd07fa788f37c6
Lukasz Machowski <lukaszmac@mweb.co.za>
2023-11-18 20:58:23 +0200

    Initial structure.

---
+++ src/test/java/io/nanovc/RepoTests.java
@@ -0,0 +1,13 @@
+package io.nanovc;
+
+import io.nanovc.memory.strings.StringNanoRepo;
+import org.junit.jupiter.api.Test;
+
+public class RepoTests
+{
+    @Test
+    public void creationTest()
+    {
+        new StringNanoRepo();
+    }
+}

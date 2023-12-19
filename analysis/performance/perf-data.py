80636b915265d3da8750917c4765382513bb2dcf
Lukasz Machowski <lukaszmac@mweb.co.za>
2023-12-19 12:50:08 +0200

    Measured performance across all algorithms.

--- analysis/performance/perf-data.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-import pandas as pd
-
-data_dir = Path(__file__).parent / 'data'
-
-csv_file = data_dir / 'HierarchicalGridDiv100Max10Tests' / '2023-11-29-21-45-01_index_Random_Linear.csv'
-
-csv_data = pd.read_csv(csv_file)

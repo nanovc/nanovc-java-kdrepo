5d52ddb2dcf4f6f3d6a23dd3ace0609dda1470b2
Lukasz Machowski <lukaszmac@mweb.co.za>
2024-01-11 13:36:06 +0200

    Added more tests.

--- src/test/java/io/nanovc/indexing/kdtree/bentley1990/KDTreeTests.java
+++ src/test/java/io/nanovc/indexing/kdtree/bentley1990/KDTreeTests.java
@@ -50,4 +50,34 @@ class KDTreeTests
         nearest = kdTree.searchNearest(new XY(2.1, 2.1));
         assertEquals(new XY(2.0, 2.0), nearest);
     }
+
+    @Test
+    public void test_X1Y1_X1YNeg1_XNeg1YNeg1_XNeg1Y1()
+    {
+        KDTree<XY> kdTree = new KDTree<>(XY::extractCoordinate, XY::measureDistanceL2NormEuclidean);
+
+        var items = new ArrayList<XY>();
+        items.add(new XY(1.0, 1.0));
+        items.add(new XY(1.0, -1.0));
+        items.add(new XY(-1.0, -1.0));
+        items.add(new XY(-1.0, 1.0));
+
+        // Build the index:
+        kdTree.index(items);
+
+        // Search for the nearest neighbour:
+        XY nearest;
+
+        nearest = kdTree.searchNearest(new XY(0.0, 0.0));
+        assertEquals(new XY(1.0, 1.0), nearest);
+
+        nearest = kdTree.searchNearest(new XY(0.5, 0.5));
+        assertEquals(new XY(1.0, 1.0), nearest);
+
+        nearest = kdTree.searchNearest(new XY(-0.5, -0.5));
+        assertEquals(new XY(-1.0, -1.0), nearest);
+
+        nearest = kdTree.searchNearest(new XY(-0.5, 0.5));
+        assertEquals(new XY(-1.0, 1.0), nearest);
+    }
 }

a7d6b7bb67e2c417cca0f43f69340313fe1de4d6
Lukasz Machowski <lukaszmac@mweb.co.za>
2024-01-12 19:45:59 +0200

    Added StringArithmetic.

---
+++ src/main/java/io/nanovc/indexing/repo/arithmetic/StringArithmetic.java
@@ -0,0 +1,117 @@
+package io.nanovc.indexing.repo.arithmetic;
+
+/**
+ * Arithmetic with {@link String strings}.
+ */
+public class StringArithmetic extends Arithmetic<String>
+{
+    /**
+     * Compares the left value to the right value.
+     * <p>
+     * You can use it like this:
+     * compare(left, right) <  0 // for left <= right
+     * compare(left, right) <= 0 // for left <= right
+     * compare(left, right) == 0 // for left == right
+     * compare(left, right) != 0 // for left != right
+     * compare(left, right) >= 0 // for left >= right
+     * compare(left, right) >  0 // for left >  right
+     *
+     * @param leftValue  The left value to compare.
+     * @param rightValue The right value to compare.
+     * @return A negative integer, zero, or a positive integer as the first argument is less than, equal to, or greater than the second.
+     */
+    @Override public int compare(String leftValue, String rightValue)
+    {
+        return leftValue.compareTo(rightValue);
+    }
+
+    /**
+     * Adds the two values.
+     * result = leftValue + rightValue
+     *
+     * @param leftValue  The left value to add.
+     * @param rightValue The right value to add.
+     * @return The result of adding the two values. result = leftValue + rightValue
+     */
+    @Override public String add(String leftValue, String rightValue)
+    {
+        return leftValue + rightValue;
+    }
+
+    /**
+     * Subtracts the two values.
+     * result = leftValue - rightValue
+     *
+     * @param leftValue  The left value to subtract.
+     * @param rightValue The right value to subtract.
+     * @return The result of subtracting the two values. result = leftValue - rightValue
+     */
+    @Override public String subtract(String leftValue, String rightValue)
+    {
+        throw new UnsupportedOperationException("Cannot subtract strings");
+    }
+
+    /**
+     * Multiplies the two values.
+     * result = leftValue * rightValue
+     *
+     * @param leftValue  The left value to multiply.
+     * @param rightValue The right value to multiply.
+     * @return The result of multiplying the two values. result = leftValue * rightValue
+     */
+    @Override public String multiply(String leftValue, String rightValue)
+    {
+        throw new UnsupportedOperationException("Cannot multiply strings");
+    }
+
+    /**
+     * Divides the two values.
+     * result = leftValue / rightValue
+     *
+     * @param leftValue  The left value to divide.
+     * @param rightValue The right value to divide.
+     * @return The result of dividing the two values. result = leftValue / rightValue
+     */
+    @Override public String divide(String leftValue, String rightValue)
+    {
+        throw new UnsupportedOperationException("Cannot divide strings");
+    }
+
+    /**
+     * Halves the value.
+     * result = value / 2
+     *
+     * @param value The value to half.
+     * @return The result of halving the value. result = value / 2
+     */
+    @Override public String halveValue(String value)
+    {
+        return value.substring(0, value.length() / 2);
+    }
+
+    /**
+     * Doubles the value.
+     * result = value * 2
+     *
+     * @param value The value to double.
+     * @return The result of doubling the value. result = value * 2
+     */
+    @Override public String doubleValue(String value)
+    {
+        return value + value;
+    }
+
+    /**
+     * The singleton reusable instance.
+     */
+    private final static StringArithmetic instance = new StringArithmetic();
+
+    /**
+     * The singleton reusable instance.
+     * @return The singleton reusable instance.
+     */
+    public static StringArithmetic instance()
+    {
+        return instance;
+    }
+}
